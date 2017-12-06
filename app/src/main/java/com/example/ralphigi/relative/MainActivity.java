052f0f2a72b8c66e843995da608aa5f0fa89e546
rafakwas <ralphigi@gmail.com>
2017-12-06 01:15:54 +0100

    ...

--- app/src/main/java/com/example/ralphigi/relative/MainActivity.java
+++ app/src/main/java/com/example/ralphigi/relative/MainActivity.java
@@ -7,20 +7,77 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.math.BigDecimal;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout mainLayout;
+
+    Button randomButton;
+    ToggleButton toggleButton;
+    SeekBar redBar;
+    SeekBar blueBar;
+    SeekBar greenBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainLayout = findViewById(R.id.relativeLayout);
+
+        toggleButton = findViewById(R.id.toggleButton);
+        randomButton = findViewById(R.id.button);
+        redBar = findViewById(R.id.redBar);
+        greenBar = findViewById(R.id.greenBar);
+        blueBar = findViewById(R.id.blueBar);
+
+        makeWhite();
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(toggleButton.isChecked()) {
+                    Toast.makeText(MainActivity.this, "Let's rock!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (toggleButton.isChecked()) {
+                    Random random = new Random();
+                    int color = Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256));
+                    mainLayout.setBackgroundColor(color);
+                }
+            }
+        });
+
+        redBar.setOnSeekBarChangeListener(new ColorBarChangeListener());
+        greenBar.setOnSeekBarChangeListener(new ColorBarChangeListener());
+        blueBar.setOnSeekBarChangeListener(new ColorBarChangeListener());
     }
 
+    private void updateBackground()
+    {
+        int red = BigDecimal.valueOf(redBar.getProgress() * 2.55).intValue();
+        int green = BigDecimal.valueOf(greenBar.getProgress() * 2.55).intValue();
+        int blue = BigDecimal.valueOf(blueBar.getProgress() * 2.55).intValue();
+        mainLayout.setBackgroundColor(
+                0xff000000
+                        + red * 0x10000
+                        + green * 0x100
+                        + blue
+        );
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,11 +86,18 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private void makeWhite() {
+        redBar.setProgress(100);
+        blueBar.setProgress(100);
+        greenBar.setProgress(100);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
                 mainLayout.setBackgroundColor(Color.argb(255,255,255,255));
+                makeWhite();
                 Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
                         .show();
                 break;
@@ -44,8 +108,17 @@ public class MainActivity extends AppCompatActivity {
             default:
                 break;
         }
-
         return true;
     }
 
+    private class ColorBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+            updateBackground();
+        }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    }
 }
